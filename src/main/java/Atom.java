6cbac990c843e4dc07c4cec865319ec304325ac1
judHoka <judha38@gmail.com>
2025-01-23 00:30:59 +0800

    Added code skeletal and introducing, Atom!

---
+++ src/main/java/Atom.java
@@ -0,0 +1,18 @@
+public class Atom {
+    public static void main(String[] args) {
+        String logo = "    ____     _                        /\\    \n"
+                    + "   / __ \\   | |__   ____   __  __  \\ /  \\ /  \n"
+                    + "  / /__\\ \\  |  __| / __ \\ |  \\/  | <      > \n"
+                    + " / /    \\ \\ | |___| |__| || |\\/| | / \\  / \\  \n"
+                    + "/_/      \\_\\ \\____|\\____/ |_|  |_|    \\/    \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hey hey there! Its your favourite chatbot assistant, Atom :D");
+        System.out.println("Is there anything I can help you with?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Alright, I'll see ya next time!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
